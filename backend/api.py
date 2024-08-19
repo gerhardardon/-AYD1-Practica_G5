7d61567e597e6b281398b4f43834b54340fa329e
gerhardardon <gerhardardon@gmail.com>
2024-08-18 22:42:15 -0600

    style(backend): Formato de api en flask y gitignore

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,14 @@
 # Aca ira la api REST hecha en flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from handlers import *
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/',methods=['GET'])
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True)
